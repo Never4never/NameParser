3ea7a2248f9d04030de92c337815d0a43234c9f5
anna <annephemere@gmail.com>
2017-10-09 16:32:17 +0300

    Deleted unnecessary methods & lines.

--- src/main/java/NameParser.java
+++ src/main/java/NameParser.java
@@ -1,5 +1,3 @@
-
-
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -39,71 +37,6 @@ public class NameParser {
         System.out.println("First Name is: " + firstName);
         return firstName;
     }
-
-
-    /**
-     * Method gets the full name, calls method nameWithoutSuffixes
-     * <p>
-     * If there is a suffix from list, it ignores it and takes the previous word from name.
-     *
-     * @param fullName the full name which was entered
-     * @return he last name from the full name
-     */
-    public String getLastName(String fullName) {
-
-        List<String> names = nameWithoutSuffixes(fullName);
-
-        System.out.println("lastname: " + names.get(names.size() - 1));
-        return names.get(names.size() - 1);
-    }
-
-
-    /**
-     * Method gets the full name, trims the string,
-     * removes extra spaces and splits the full name into parts.
-     * <p>
-     * If there is a suffix from list, returns true.
-     *
-     * @param fullName the full name which was entered
-     * @return the result of comparison -- if full name cotains any suffixes
-     */
-    public boolean ifSuffixExists(String fullName) {
-
-        Set<String> nameInParts = new HashSet<>(Arrays.asList(fullName.trim().toLowerCase().replaceAll("\\s+", " ").split(" ")));
-
-        boolean isMatch = Arrays.stream(KnownSuffixes.KNOWN_SUFFIXES)
-                .flatMap(Arrays::stream)
-                .anyMatch(s -> nameInParts.contains(s)); //compares the whole nameInParts with suffixes list
-
-        System.out.println("Suffix existing: " + isMatch);
-
-        return isMatch;
-    }
-
-
-    /**
-     * Method gets the full name, split into parts by space,
-     * trims the String
-     * takes ones without spaces
-     * and compares with the list of suffixes.
-     * Then collects the result into the list.
-     *
-     * @param fullName the full name which was entered
-     * @return list of parts of name without any suffixes
-     */
-    public List<String> nameWithoutSuffixes(String fullName) {
-
-        return Arrays.stream(fullName.split(" "))
-                .map(String::trim)
-                .filter(s -> s.length() > 0)
-                .filter(s -> Arrays.stream(KnownSuffixes.KNOWN_SUFFIXES)
-                        .flatMap(Arrays::stream)
-                        .noneMatch(s::equalsIgnoreCase))
-                .collect(Collectors.toList());
-
-    }
-
-
 
     public boolean matchName(String lastName, String fullName) {
         List last = parseInput(lastName);
@@ -160,29 +93,8 @@ public class NameParser {
     }
 
 
-
-    public static String ifMergedSuffixes (String name){
-
-        String numbers[] = {"1","2","3","4","5","6","7","8","9"};
-
-        for (int i = 0; i<= numbers.length-1; i++) {
-            if (name.contains(numbers[i])) {
-
-                name = name.replace(" "+numbers[i], numbers[i]);
-            }
-        }
-        return name;
-    }
-
-
-
     public static void main(String[] args) {
         NameParser parser = new NameParser();
-        parser.ifSuffixExists(fullName);
-        parser.getLastName(fullName);
-        System.out.println(String.join(" ", parser.nameWithoutSuffixes(fullName)));
 
-        parser.matchName(lastName, fullName);
-        System.out.println(ifMergedSuffixes(fullName).equalsIgnoreCase(ifMergedSuffixes(lastName)));
-
+    }
 }
